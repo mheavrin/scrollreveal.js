8bb2e5a235ae0293902da96f5d67bdc990ba74ad
M.Heavrin <m.heavrin@rto.de>
2016-03-22 11:53:32 +0100

    Add configuration option to ignore horizontal scrolling.

--- dist/scrollreveal.js
+++ dist/scrollreveal.js
@@ -53,54 +53,57 @@
 
         ScrollReveal.prototype.defaults = {
 
-            //            'bottom', 'left', 'top', 'right'
-            origin      : 'bottom',
+            //                'bottom', 'left', 'top', 'right'
+            origin          : 'bottom',
 
-            //            Can be any valid CSS distance, e.g. '5rem', '10%', '20vw', etc.
-            distance    : '20px',
+            //                Can be any valid CSS distance, e.g. '5rem', '10%', '20vw', etc.
+            distance        : '20px',
 
-            //            Time in milliseconds.
-            duration    : 500,
-            delay       : 0,
+            //                Time in milliseconds.
+            duration        : 500,
+            delay           : 0,
 
-            //            Starting angles in degrees, will transition from these values to 0 in all axes.
-            rotate      : { x: 0, y: 0, z: 0 },
+            //                Starting angles in degrees, will transition from these values to 0 in all axes.
+            rotate          : { x: 0, y: 0, z: 0 },
 
-            //            Starting opacity value, before transitioning to the computed opacity.
-            opacity     : 0,
+            //                Starting opacity value, before transitioning to the computed opacity.
+            opacity         : 0,
 
-            //            Starting scale value, will transition from this value to 1
-            scale       : 0.9,
+            //                Starting scale value, will transition from this value to 1
+            scale           : 0.9,
 
-            //            Accepts any valid CSS easing, e.g. 'ease', 'ease-in-out', 'linear', etc.
-            easing      : 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+            //                Accepts any valid CSS easing, e.g. 'ease', 'ease-in-out', 'linear', etc.
+            easing          : 'cubic-bezier(0.6, 0.2, 0.1, 1)',
 
-            //            When null, `<html>` is assumed to be the reveal container. You can pass a
-            //            DOM node as a custom container, e.g. document.querySelector('.fooContainer')
-            //            or a selector, e.g. '.fooContainer'
-            container   : null,
+            //                When null, `<html>` is assumed to be the reveal container. You can pass a
+            //                DOM node as a custom container, e.g. document.querySelector('.fooContainer')
+            //                or a selector, e.g. '.fooContainer'
+            container       : null,
 
-            //            true/false to control reveal animations on mobile.
-            mobile      : true,
+            //                true/false to control reveal animations on mobile.
+            mobile          : true,
 
-            //            true:  reveals occur every time elements become visible
-            //            false: reveals occur once as elements become visible
-            reset       : false,
+            //                true:  reveals occur every time elements become visible
+            //                false: reveals occur once as elements become visible
+            reset           : false,
 
-            //            'always' — delay for all reveal animations
-            //            'once'   — delay only the first time reveals occur
-            //            'onload' - delay only for animations triggered by first load
-            useDelay    : 'always',
+            //                'always' — delay for all reveal animations
+            //                'once'   — delay only the first time reveals occur
+            //                'onload' - delay only for animations triggered by first load
+            useDelay        : 'always',
 
-            //            Change when an element is considered in the viewport. The default value
-            //            of 0.20 means 20% of an element must be visible for its reveal to occur.
-            viewFactor  : 0.2,
+            //                Change when an element is considered in the viewport. The default value
+            //                of 0.20 means 20% of an element must be visible for its reveal to occur.
+            viewFactor      : 0.2,
 
-            //            Pixel values that alter the container boundaries.
-            //            e.g. Set `{ top: 48 }`, if you have a 48px tall fixed toolbar.
-            //            --
-            //            Visual Aid: https://scrollrevealjs.org/assets/viewoffset.png
-            viewOffset  : { top: 0, right: 0, bottom: 0, left: 0 },
+            //                Pixel values that alter the container boundaries.
+            //                e.g. Set `{ top: 48 }`, if you have a 48px tall fixed toolbar.
+            //                --
+            //                Visual Aid: https://scrollrevealjs.org/assets/viewoffset.png
+            viewOffset      : { top: 0, right: 0, bottom: 0, left: 0 },
+
+            //                Ignore horizontal calculations for _isElemVisible
+            ignoreHorizontal: false,
 
             //            Callbacks that fire for each completed element reveal, and if
             //            `config.reset = true`, for each completed element reset. When creating your
@@ -854,10 +857,17 @@
                     viewBottom = scrolled.y - elem.config.viewOffset.bottom + container.height,
                     viewRight  = scrolled.x - elem.config.viewOffset.right  + container.width;
 
-                return top    < viewBottom
+                if (!elem.config.ignoreHorizontal) {
+                    return top    < viewBottom
+                      && bottom > viewTop
+                      && left   > viewLeft
+                      && right  < viewRight
+                }
+                else {
+                    return top < viewBottom
                     && bottom > viewTop
-                    && left   > viewLeft
-                    && right  < viewRight
+                }
+
             }
 
             function isPositionFixed() {
